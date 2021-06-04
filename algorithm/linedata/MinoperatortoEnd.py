4a507fe730064586643f39017965991c64450918
zhangjn <2443464529@qq.com>
2021-06-04 06:57:18 -0700

    中缀表达式转后缀表达式通用算法（可拓展云算符）

--- algorithm/linedata/MinoperatortoEnd.py
+++ algorithm/linedata/MinoperatortoEnd.py
@@ -6,3 +6,49 @@ Created on Thu Jun  3 20:38:45 2021
 @author: zhangjn
 """
 
+import sys
+sys.path.append('/home/zhangjn/software/pythonSpace/algorithm/')
+from pythonds.basic.stack.myStack import Stack
+
+def infixToPostfix(infixexpr):
+#   print(infixexpr)
+    prec = {}
+    prec['*'] = 3
+    prec['/'] = 3
+    prec['+'] = 2
+    prec['-'] = 2
+    prec['('] = 1
+    opStack = Stack()
+    postfixList = []
+    tokenList = infixexpr.split()
+#   print(tokenList)
+    for token in tokenList:
+        if token in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' or \
+            token in '0123456789':
+                postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            topToken = opStack.pop()
+            while topToken != '(':
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and \
+                (prec[opStack.peek()] >= prec[token]):
+                    postfixList.append(opStack.pop())
+            opStack.push(token)
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+    return ''.join(postfixList)
+
+print(infixToPostfix('A + B * C + D'))
+print(infixToPostfix('( ( E + F ) * ( M + N ) )'))
+print(infixToPostfix('( E + F ) * ( M + N )'))
+    
+    
+    
+    
+    
+    
+    
